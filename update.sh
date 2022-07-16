3d6dc614a011e9d4a356a882bac186738c07d101
Geraldo Homero <geraldohomero@protonmail.com>
2022-07-16 18:44:28 -0300

    Add files via upload

---
+++ update.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+VERMELHO='\e[1;91m'
+VERDE='\e[1;92m'
+SEM_COR='\e[0m'
+#----#
+# Internet conectando?
+if ! ping -c 1 8.8.8.8 -q &> /dev/null; then
+  echo -e "${VERMELHO}[ERROR] - Seu computador não tem conexão com a Internet.${SEM_COR}"
+  exit 1
+else
+  echo -e "${VERDE}[INFO] - Conexão com a Internet verificada${SEM_COR}"
+fi
+
+upgrade_limpeza () {
+  echo -e "${VERDE}[INFO] - Fazendo upgrade e limpeza...${SEM_COR}"
+  sleep 2
+  sudo apt autoclean
+  sudo apt clean
+  sudo apt update -m
+  sudo dpkg --configure -a
+  sudo apt install -f
+  sudo apt full-upgrade
+  sudo apt autoremove -y 
+  sudo flatpak update
+  #atualiza flatpaks
+}  
+
+#----# Execução
+upgrade_limpeza
